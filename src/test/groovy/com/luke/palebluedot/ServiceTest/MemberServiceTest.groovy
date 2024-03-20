5433fda3ba8714338224df2fd4d7259ff09d8680
luke-jeong <jgh3482@gmail.com>
2024-03-20 13:36:09 +0900

    memberedit test 추가

--- src/test/groovy/com/luke/palebluedot/ServiceTest/MemberServiceTest.groovy
+++ src/test/groovy/com/luke/palebluedot/ServiceTest/MemberServiceTest.groovy
@@ -3,32 +3,58 @@ package com.luke.palebluedot.ServiceTest
 import com.luke.palebluedot.domain.Member
 import com.luke.palebluedot.repository.MemberRepository
 import com.luke.palebluedot.request.MemberCreate
+import com.luke.palebluedot.request.MemberEdit
 import com.luke.palebluedot.service.MemberService
 import spock.lang.Specification
 
-
 public class MemberServiceTest extends Specification {
-
-    MemberService memberService
     MemberRepository memberRepository = Mock()
+    MemberService memberService = new MemberService(memberRepository)
 
-    def setup(){
-        memberService = new MemberService(memberRepository)
+    def createMember(Long memberId) {
+        Member member = new Member(
+                memberId: memberId,
+                memberName: "testMember",
+                password: "testPassword",
+                email: "test@example.com"
+        )
+        return member
     }
+
 
     def "createMember - 정상작동확인"(){
         given:
-        MemberCreate member = new MemberCreate()
-        member.setMemberName("testName")
-        member.setPassword("testpwd")
-        member.setEmail("test@mail.com")
+        Long memberId = 1L
+        MemberCreate joinMember = createMember(memberId)
 
         when:
-        memberService.createMember(member)
+        memberService.createMember(joinMember)
 
         then:
         1*memberRepository.save(_) >> {args ->
             def param = args[0] as Member
         }
+    }
+
+
+    def "editMember - 정상작동확인"(){
+        given:
+        Long memberId = 1L
+        Member existMember = createMember(memberId)
+        memberRepository.findById(memberId) >> Optional.of(existMember)
+        memberRepository.save(_ as Member) >> { Member savedMember -> savedMember }
+
+        MemberEdit memberEdit = MemberEdit.builder()
+            .memberName("editname")
+            .email("editmail")
+            .password("editpassword")
+            .build()
+
+        when:
+        Member result = memberService.editMember(memberId, memberEdit)
+
+        then:
+        result != null
+        result.memberName == memberEdit.memberName
     }
 }
